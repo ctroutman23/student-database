1f8725a8a05aea2b6d1836fd42516e49985529e6
carson-troutman <carson.troutman@gmail.com>
2024-12-29 16:36:04 -0500

    created db and students table

---
+++ mysql.sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE students(student_id INTEGER NOT NULL PRIMARY KEY, 
+-- student_name TEXT NOT NULL, age INTEGER NOT NULL, gender TEXT NOT NULL, 
+-- grade_level INTEGER NOT NULL, attendance INTEGER NOT NULL, gpa FLOAT NOT NULL); 
+
+-- INSERT INTO students(student_id, student_name, age, gender,
+-- grade_level, attendance, gpa)
+-- VALUES (1, "Joe Miller", 17, "M", 11, 10, 3.4 );
+
+SELECT * FROM students;
